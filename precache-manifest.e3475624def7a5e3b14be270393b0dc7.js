3136410a76fd2b7528179571814342132917ecdb
unknown <archita.bhatnagar07@gmail.com>
2022-01-24 11:21:26 +0530

    Updates

---
+++ precache-manifest.e3475624def7a5e3b14be270393b0dc7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77e6232e8dedeff0769d121ffdee3545",
+    "url": "/React_Keeper/index.html"
+  },
+  {
+    "revision": "20fbc0d7d55685462a06",
+    "url": "/React_Keeper/static/js/2.c416884b.chunk.js"
+  },
+  {
+    "revision": "11bffcc15966e8e13b4c",
+    "url": "/React_Keeper/static/js/main.3c8cbe1f.chunk.js"
+  },
+  {
+    "revision": "f969e096f55ae8d8a5ff",
+    "url": "/React_Keeper/static/js/runtime-main.2205772d.js"
+  }
+]);
